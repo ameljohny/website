81239f526fec43a91b9e526c51333bf4ba972e0f
ameljohny <amel.johny@gmail.com>
2018-05-02 14:28:38 +0530

    livepae without counter

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { LiveComponent } from './components/live/live.component';
+import { AccordionComponent } from './components/accordion/accordion.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    LiveComponent
+    LiveComponent,
+    AccordionComponent
   ],
   imports: [
     BrowserModule
