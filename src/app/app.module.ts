76d376004e0ef9e5b06246c57386b72ffb82991a
amaljoseami <amaljoseami@gmail.com>
2018-05-03 18:45:05 +0530

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { LiveComponent } from './components/live/live.component';
+import { NavHorizontalComponent } from './components/nav-horizontal/nav-horizontal.component';
+import { NavVerticalComponent } from './components/nav-vertical/nav-vertical.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    LiveComponent
+    LiveComponent,
+    NavHorizontalComponent,
+    NavVerticalComponent
   ],
   imports: [
     BrowserModule
