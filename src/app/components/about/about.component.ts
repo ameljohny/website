2725228e14d710d6ddfc2958d7e43676a0aea74e
amaljoseami <amaljoseami@gmail.com>
2018-05-14 14:53:02 +0530

    half

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-about',
@@ -10,6 +11,18 @@ export class AboutComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    document.getElementById("lefthead1").style.display="none";
+    document.getElementById("leftpara1").style.display="none";
+
+
+    document.getElementById("lefthead3").style.display="none";
+    document.getElementById("leftpara3").style.display="none";
+
+    document.getElementById("lefthead4").style.display="none";
+    document.getElementById("leftpara4").style.display="none";
+
   }
 
 }
+
+
