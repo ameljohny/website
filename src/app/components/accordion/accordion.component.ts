53c98ea345ac85d1b6382e238f208571373c5008
Amel Johny <amel.johny@gmail.com>
2018-05-17 13:08:08 +0530

    merging for dopie responsive final

--- src/app/components/accordion/accordion.component.ts
+++ src/app/components/accordion/accordion.component.ts
@@ -13,10 +13,10 @@ export class AccordionComponent implements OnInit {
     const header = document.getElementById('accordion');
     const tabs = header.getElementsByClassName('tabs');
     for (let i = 0; i < tabs.length; i++) {
-    tabs[i].addEventListener('mouseover', function() {
-    const current = document.getElementsByClassName('col-md-5');
-    current[0].className = current[0].className.replace('active col-lg-5 col-md-5', 'col-lg-1 col-md-1');
-    this.className = this.className.replace('col-lg-1 col-md-1', 'active col-lg-5 col-md-5');
+    tabs[i].addEventListener('click', function() {
+    const current = document.getElementsByClassName('activetabs');
+    current[0].className = current[0].className.replace('activetabs', 'inactivetabs');
+    this.className = this.className.replace('inactivetabs', 'activetabs');
     });
   }
   }
