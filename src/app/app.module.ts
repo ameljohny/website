6afccae121aa66c7da1bd2adbd44fd58653758e2
Akhil R P <mail.abhijithms@gmail.com>
2018-05-17 06:57:27 +0530

    test3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,16 +8,9 @@ import { NavHorizontalComponent } from './components/nav-horizontal/nav-horizont
 import { NavVerticalComponent } from './components/nav-vertical/nav-vertical.component';
 import { AccordionComponent } from './components/accordion/accordion.component';
 import { LandingpageComponent } from './components/landingpage/landingpage.component';
-<<<<<<< HEAD
-import { CounterComponent } from './components/counter/counter.component';
-import { GalleryComponent } from './components/gallery/gallery.component';
-import { FooterComponent } from './components/footer/footer.component';
-
-=======
 import { FooterComponent } from './components/footer/footer.component';
 import { VideoBackgroundComponent } from './components/video-background/video-background.component';
 import { AboutComponent } from './components/about/about.component';
->>>>>>> 40de5d038634f6008a682a2be462f39686311f43
 
 
 @NgModule({
@@ -28,17 +21,10 @@ import { AboutComponent } from './components/about/about.component';
     NavVerticalComponent,
     AccordionComponent,
     LandingpageComponent,
-<<<<<<< HEAD
-    CounterComponent,
-    GalleryComponent,
-    FooterComponent
-  ],
-=======
     FooterComponent,
     VideoBackgroundComponent,
     AboutComponent
 ],
->>>>>>> 40de5d038634f6008a682a2be462f39686311f43
   imports: [
     BrowserModule
   ],
