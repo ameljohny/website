80bda3914f813d76ad93ff206877ee1fa22d8b6f
amaljoseami <amaljoseami@gmail.com>
2018-05-21 15:39:42 +0530

    bug fix-vnav, bg clr ok- loading, landing page components  removed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,8 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-import { LiveComponent } from './components/live/live.component';
 import { NavHorizontalComponent } from './components/nav-horizontal/nav-horizontal.component';
 import { NavVerticalComponent } from './components/nav-vertical/nav-vertical.component';
-import { AccordionComponent } from './components/accordion/accordion.component';
-import { LandingpageComponent } from './components/landingpage/landingpage.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { VideoBackgroundComponent } from './components/video-background/video-background.component';
 import { AboutComponent } from './components/about/about.component';
@@ -17,11 +14,8 @@ import { LoadingComponent } from './components/loading/loading.component';
 @NgModule({
   declarations: [
     AppComponent,
-    LiveComponent,
     NavHorizontalComponent,
     NavVerticalComponent,
-    AccordionComponent,
-    LandingpageComponent,
     FooterComponent,
     VideoBackgroundComponent,
     AboutComponent,
