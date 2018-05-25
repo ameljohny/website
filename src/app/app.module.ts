22de8dc4085b4cc8b27afaf29b7a53f2110e2dc1
Amel Johny <amel.johny@gmail.com>
2018-05-25 18:28:40 +0530

    without video aot

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,24 +6,20 @@ import {Ng2PageScrollModule} from 'ng2-page-scroll';
 
 import { LoadingComponent } from './components/loading/loading.component';
 import { LandingpageComponent } from './components/landingpage/landingpage.component';
-import { VideoBackgroundComponent } from './components/video-background/video-background.component';
+
 
 import { AboutComponent } from './components/about/about.component';
 import { NavVerticalComponent } from './components/nav-vertical/nav-vertical.component';
 
 import { ContactComponent } from './components/contact/contact.component';
 import { NavVertContactComponent } from './components/nav-vert-contact/nav-vert-contact.component';
-
-
-import { LiveComponent } from './components/live/live.component';
-import { CounterComponent } from './components/counter/counter.component';
-import { AccordionComponent } from './components/accordion/accordion.component';
+import { RouterModule, Routes } from '@angular/router';
 
 import { GalleryComponent } from './components/gallery/gallery.component';
 import { FooterComponent } from './components/footer/footer.component';
-import { RouterModule, Routes } from '@angular/router';
+
 import { Page1Sec2Component } from './components/page1-sec2/page1-sec2.component';
-import { Page1Sec3Component } from './components/page1-sec3/page1-sec3.component'; 
+import { Page1Sec3Component } from './components/page1-sec3/page1-sec3.component';
 
 
 const appRoutes: Routes = [
@@ -40,18 +36,15 @@ const appRoutes: Routes = [
     AppComponent,
     LoadingComponent,
     LandingpageComponent,
-    VideoBackgroundComponent,
     AboutComponent,
     ContactComponent,
     FooterComponent,
-    LiveComponent,
-    AccordionComponent,
     NavVerticalComponent,
     NavVertContactComponent,
-    CounterComponent,
     GalleryComponent,
     Page1Sec2Component,
-    Page1Sec3Component
+    Page1Sec3Component,
+
    
 ],
   imports: [
