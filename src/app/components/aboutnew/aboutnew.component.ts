74ac3f6623bc7b5f0eaf9d8fd5f59fd95ad19910
amaljoseami <amaljoseami@gmail.com>
2018-05-28 22:34:49 +0530

    about set

--- src/app/components/aboutnew/aboutnew.component.ts
+++ src/app/components/aboutnew/aboutnew.component.ts
@@ -11,28 +11,6 @@ export class AboutnewComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    const script = $(document).ready(function() {
-      $('.js-anchor-link').click(function(e){
-     e.preventDefault();
-     const target = $($(this).attr('href'));
-     if( target.length){
-       const scrollTo = target.offset().top;
-       $('body, html').animate({scrollTop: scrollTo-100+'px'}, 1500);
-     }
-   });
-  });
-
-
-    // Add active class to the current button (highlight it)
-    var header = document.getElementById("verticallinks");
-    var btns = header.getElementsByClassName("js-anchor-link");
-    for (var i = 0; i < btns.length; i++) {
-      btns[i].addEventListener("click", function() {
-        var current = document.getElementsByClassName("aboutactive");
-        current[0].className = current[0].className.replace(" aboutactive", "");
-        this.className += " aboutactive";
-      });
-    }
   }
 
 }
